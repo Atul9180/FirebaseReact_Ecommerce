ea5eb1b8864a046ba8b756f8c0ae489f229e9253
Atul <patelatulpatel@gmail.com>
2023-12-07 04:14:57 +0530

    Add and  Update Product Form Ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Order from "./pages/order/Order";
 import Cart from "./pages/cart/Cart";
-import Dashboard from "./pages/admin/dashboard/Dashboard";
 import NoPage from "./pages/nopage/NoPage";
 import ProductDetails from "./pages/products/ProductDetails";
 import UserSignUp from "./pages/Authentication/UserSignUp";
 import UserSignIn from "./pages/Authentication/UserSignIn";
+import Dashboard from "./pages/admin/dashboard/Dashboard";
+import AddProduct from "./pages/admin/productPages/AddProduct";
+import UpdateProduct from "./pages/admin/productPages/UpdateProduct";
 
 const App = () => {
   return (
@@ -20,6 +22,8 @@ const App = () => {
         <Route path="/login" element={<UserSignIn />} />
         <Route path="/signup" element={<UserSignUp />} />
         <Route path="/productinfo/:id" element={<ProductDetails />} />
+        <Route path="/addproduct" element={<AddProduct />} />
+        <Route path="/updateproduct" element={<UpdateProduct />} />
         <Route path="/*" element={<NoPage />} />
       </Routes>
     </Router>
