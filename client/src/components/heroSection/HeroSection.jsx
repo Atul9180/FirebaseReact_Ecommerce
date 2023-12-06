d58a7e97f7b2cfd4c22e78ea8724fbdd623e0c6a
Atul <patelatulpatel@gmail.com>
2023-12-06 19:14:04 +0530

    HeroSection and Filter ui

---
+++ client/src/components/heroSection/HeroSection.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+// import banner2 from "../../assets/banner2.jpg";
+
+const HeroSection = () => {
+  return (
+    <div>
+      <img
+        src="https://static.vecteezy.com/system/resources/previews/004/299/835/original/online-shopping-on-phone-buy-sell-business-digital-web-banner-application-money-advertising-payment-ecommerce-illustration-search-free-vector.jpg"
+        alt=""
+      />
+      {/* <img src={banner2} className={"w-full"} alt="heroimg" /> */}
+    </div>
+  );
+};
+
+export default HeroSection;
